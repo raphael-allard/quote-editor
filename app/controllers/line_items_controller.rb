0333c50b9aa944d671a777877348ae0c65b64924
Raphael Allard <raphael.s.allard@gmail.com>
2024-02-28 13:44:40 +0100

    added turbo to LineItem actions

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date
+  before_action :set_line_item, only: [:edit, :update, :destroy]
 
   def new
     @line_item = @line_item_date.line_items.build
@@ -10,9 +11,35 @@ class LineItemsController < ApplicationController
     @line_item = @line_item_date.line_items.build(line_item_params)
 
     if @line_item.save
-      redirect_to quote_path(@quote), notice: "Item was successfully created"
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully created" }
+        format.turbo_stream { flash.now[:notice] = "Item was successfully created" }
+      end
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @line_item.update(line_item_params)
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully updated" }
+        format.turbo_stream { flash.now[:notice] = "Item was successfully updated" }
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Item was successfully destroyed" }
+      format.turbo_stream { flash.now[:notice] = "Item was successfully destroyed" }
     end
   end
 
@@ -24,6 +51,10 @@ class LineItemsController < ApplicationController
 
   def set_line_item_date
     @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+  end
+
+  def set_line_item
+    @line_item = @line_item_date.line_items.find(params[:id])
   end
 
   def line_item_params
